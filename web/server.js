4e7e0fb065fc15bab0f75ffa3c3babaf61b4ecc9
zereneh <zereneh@ryerson.ca>
2021-12-23 08:18:12 -0500

    added docker files

--- web/server.js
+++ web/server.js
@@ -1,15 +1,17 @@
 'use strict';
 
 const express = require('express');
+var os = require('os');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const HOSTNAME = os.hostname();
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send(`CN8822 - Server/Node:${HOST}`)
+  res.send(`CN8822 - Server/Node:${HOSTNAME}`)
 });
 
 app.listen(PORT, HOST);
